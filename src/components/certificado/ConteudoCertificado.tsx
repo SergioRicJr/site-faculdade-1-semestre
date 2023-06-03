c46f1d2bd68eaae522978d0a2b6a3c058d52e362
Sergio <sergioricjr7@gmail.com>
2023-06-02 22:08:16 -0300

    terminar de inserir cursos

--- src/components/certificado/ConteudoCertificado.tsx
+++ src/components/certificado/ConteudoCertificado.tsx
@@ -4,9 +4,10 @@ import {MdCheckCircle} from 'react-icons/md'
 
 const ConteudoCertificado = ({listaConteudos, cargaHoraria}: any) => {
   return (
-    <div style={{width: '170px'}}>
+    <div style={{width: '200px'}}>
         <div style={{display: 'flex', gap: "5px"}}>
-            <h1 style={{flex: 1}}>Conteúdos</h1>
+            <h1 style={{fontSize: "16px",width: "50%", borderRight: "1px solid white", display: "flex", alignItems: "center", justifyContent: "center"}}>Conteúdos</h1>
+            <p style={{width: "50%"}}>Carga Horária: {cargaHoraria}</p>
         </div>
         <Divider/>
         <List spacing={2}>
